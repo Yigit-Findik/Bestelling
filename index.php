18cfacfcaac54dce042a888396057f684bd16fa2
Yigit <71589865+Yigit-Findik@users.noreply.github.com>
2021-04-11 01:45:37 +0200

    added tailwind and tried doing sessions

--- index.php
+++ index.php
@@ -1,157 +1,167 @@
+<?php
+    session_start();
+
+    error_reporting(E_ERROR | E_PARSE);
+?>
+
+
 <!DOCTYPE html>
 <html lang="nl">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <link rel="stylesheet" href="scripts/css/tailwind.css"> -->
         <link rel="stylesheet" href="scripts/css/styles.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
     </head>
     <body>
+        <div class="containerHEADER">
+            <h1 class="headerCLASS">Yigit's Bangers</h1>
+        </div>
+        <div class="containerFORM">
+            <div class="containerFORMINNER">
+                <form action="index.php" id="formID" method="get">
 
-        <form action="index.php" id="formID" method="get">
+                    <!-- User input, waarde -->
+                    <?php
+                        echo ('<label>Voornaam: </label><br>');
+                        if (isset($_GET['voornaam'])){
+                            if($_GET['voornaam'] == ""){
+                                echo('<input style="border-color: red" type="text" id="vnaam" name="voornaam" placeholder="Mark"> <br>');
+                            }
+                            else {
+                                echo ('<input type="text" id="vnaam" name="voornaam" placeholder="Mark"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="text" id="vnaam" name="voornaam" placeholder="Mark"> <br>');
+                        }
+                        echo ('<label>Achternaam: </label><br>');
+                        if (isset($_GET['achternaam'])){
+                            if($_GET['achternaam'] == ""){
+                                echo('<input style="border-color: red" type="text" id="anaam" name="achternaam" placeholder="Rutte"> <br>');
+                            }
+                            else {
+                                echo ('<input type="text" id="anaam" name="achternaam" placeholder="Rutte"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="text" id="anaam" name="achternaam" placeholder="Rutte"> <br>');
+                        }
+                        echo ('<label>Email: </label><br>');
+                        if (isset($_GET['email'])){
+                            if($_GET['email'] == ""){
+                                echo('<input style="border-color: red" type="email" id="mail" name="email" placeholder="voorbeeld@outlook.com"> <br>');
+                            }
+                            else {
+                                echo ('<input type="email" id="mail" name="email" placeholder="voorbeeld@outlook.com"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="email" id="mail" name="email" placeholder="voorbeeld@outlook.com"> <br>');
+                        }
+                        echo ('<label>Straat naam: </label><br>');
+                        if (isset($_GET['straatnaam'])){
+                            if($_GET['straatnaam'] == ""){
+                                echo('<input style="border-color: red" type="text" id="snaam" name="straatnaam" placeholder="straatnaam"> <br>');
+                            }
+                            else {
+                                echo ('<input type="text" id="snaam" name="straatnaam" placeholder="straatnaam"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="text" id="snaam" name="straatnaam" placeholder="straatnaam"> <br>');
+                        }
+                        echo ('<label>Huisnummer: </label><br>');
+                        if (isset($_GET['huisnummer'])){
+                            if($_GET['huisnummer'] == ""){
+                                echo('<input style="border-color: red" type="number" id="hnummer" name="huisnummer" placeholder="50"> <br>');
+                            }
+                            else {
+                                echo ('<input type="number" id="hnummer" name="huisnummer" placeholder="50"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="number" id="hnummer" name="huisnummer" placeholder="50"> <br>');
+                        }
+                        echo ('<label>Postcode: </label><br>');
+                        if (isset($_GET['postcode'])){
+                            if($_GET['postcode'] == ""){
+                                echo('<input minlength = "6" maxlength="6" style="border-color: red" type="text" id="pcode" name="postcode" placeholder="bv. 1234AA"> <br>');
+                            }
+                            else {
+                                echo ('<input minlength = "6" maxlength="6" type="text" id="pcode" name="postcode" placeholder="bv. 1234AA"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input minlength = "6" maxlength="6" type="text" id="pcode" name="postcode" placeholder="bv. 1234AA"> <br>');
+                        }
+                        echo ('<label>Woonplaats: </label><br>');
+                        if (isset($_GET['woonplaats'])){
+                            if($_GET['woonplaats'] == ""){
+                                echo('<input style="border-color: red" type="text" id="wplaats" name="woonplaats" placeholder="Nijmegen"> <br>');
+                            }
+                            else {
+                                echo ('<input type="text" id="wplaats" name="woonplaats" placeholder="Nijmegen"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="text" id="wplaats" name="woonplaats" placeholder="Nijmegen"> <br>');
+                        }
+                        echo ('<label>Telefoon nummer: </label><br>');
+                        if (isset($_GET['telefoonNummer'])){
+                            if($_GET['telefoonNummer'] == ""){
+                                echo('<input style="border-color: red" type="text" id="tnummer" name="telefoonNummer" placeholder="06 12345678"> <br>');
+                            }
+                            else {
+                                echo ('<input type="text" id="tnummer" name="telefoonNummer" placeholder="06 12345678"> <br>');
+                            }
+                        }
+                        else {
+                            echo ('<input type="text" id="tnummer" name="telefoonNummer" placeholder="06 12345678"> <br>');
+                        }
+                    ?>
 
-            <!-- User input, waarde -->
-            <label>Voornaam: </label><br>
-            <?php
-            if (isset($_GET['voornaam'])){
-                if($_GET['voornaam'] == ""){
-                    echo('<input style="border-color: red" type="text" id="vnaam" name="voornaam" placeholder="Mark"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="vnaam" name="voornaam" placeholder="Mark"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="vnaam" name="voornaam" placeholder="Mark"> <br>');
-            }
-            ?>
-            <label>Achternaam: </label><br>
-            <?php
-            if (isset($_GET['achternaam'])){
-                if($_GET['achternaam'] == ""){
-                    echo('<input style="border-color: red" type="text" id="anaam" name="achternaam" placeholder="Rutte"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="anaam" name="achternaam" placeholder="Rutte"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="anaam" name="achternaam" placeholder="Rutte"> <br>');
-            }
-            ?>
-            <label>Email: </label><br>
-            <?php
-            if (isset($_GET['email'])){
-                if($_GET['email'] == ""){
-                    echo('<input style="border-color: red" type="text" id="mail" name="email" placeholder="voorbeeld@outlook.com"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="mail" name="email" placeholder="voorbeeld@outlook.com"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="mail" name="email" placeholder="voorbeeld@outlook.com"> <br>');
-            }
-            ?>
-            <label>Straat naam: </label><br>
-            <?php
-            if (isset($_GET['straatnaam'])){
-                if($_GET['straatnaam'] == ""){
-                    echo('<input style="border-color: red" type="text" id="snaam" name="straatnaam" placeholder="straatnaam"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="snaam" name="straatnaam" placeholder="straatnaam"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="snaam" name="straatnaam" placeholder="straatnaam"> <br>');
-            }
-            ?>
-            <label>Huisnummer: </label><br>
+                    <!-- Gerechten -->
+                    <label>Gerechten: </label><br>
+                    <select name="gerecht">
+                        <option value="Gehaktbal">Gehaktbal</option>
+                        <option value="Frikandel">Frikandel</option>
+                        <option value="Kapsalon" >Kapsalon</option>
+                        <option value="SBurger" >Salar burger</option>
+                        <option value="Mexicano" >Mexicano</option>
+                        <option value="Druif" >Druif</option>
+                    </select><br>
 
-            <?php
-            if (isset($_GET['huisnummer'])){
-                if($_GET['huisnummer'] == ""){
-                    echo('<input style="border-color: red" type="text" id="hnummer" name="huisnummer" placeholder="50"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="hnummer" name="huisnummer" placeholder="50"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="hnummer" name="huisnummer" placeholder="50"> <br>');
-            }
-            ?>
-            <label>Postcode: </label><br>
-            <?php
-            if (isset($_GET['postcode'])){
-                if($_GET['postcode'] == ""){
-                    echo('<input style="border-color: red" type="text" id="pcode" name="postcode" placeholder="bv. 1234AA"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="pcode" name="postcode" placeholder="bv. 1234AA"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="pcode" name="postcode" placeholder="bv. 1234AA"> <br>');
-            }
-            ?>
-            <label>Woonplaats: </label><br>
-            <?php
-            if (isset($_GET['woonplaats'])){
-                if($_GET['woonplaats'] == ""){
-                    echo('<input style="border-color: red" type="text" id="wplaats" name="woonplaats" placeholder="Nijmegen"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="wplaats" name="woonplaats" placeholder="Nijmegen"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="wplaats" name="woonplaats" placeholder="Nijmegen"> <br>');
-            }
-            ?>
-            <label>Telefoon nummer: </label><br>
-            <?php
-            if (isset($_GET['telefoonNummer'])){
-                if($_GET['telefoonNummer'] == ""){
-                    echo('<input style="border-color: red" type="text" id="tnummer" name="telefoonNummer" placeholder="06 12345678"> <br>');
-                }
-                else {
-                    echo ('<input type="text" id="tnummer" name="telefoonNummer" placeholder="06 12345678"> <br>');
-                }
-            }
-            else {
-                echo ('<input type="text" id="tnummer" name="telefoonNummer" placeholder="06 12345678"> <br>');
-            }
-            ?>
+                    <!-- Dranken -->
+                    <label>Dranken: </label><br>
+                    <select name="dranken" id="">
+                        <option value="Water">Water</option>
+                        <option value="CHMelk">Chocolade melk</option>
+                        <option value="Fristi">Fristi</option>
+                        <option value="Cola">Cola</option>
+                        <option value="Fanta">Fanta</option>
+                        <option value="Melk">Melk</option>
+                    </select><br>
 
-            <!-- Gerechten -->
-            <label>Gerechten: </label><br>
-            <select name="gerecht">
-                <option value="Gehaktbal">Gehaktbal</option>
-                <option value="Frikandel">Frikandel</option>
-                <option value="Kapsalon" >Kapsalon</option>
-            </select><br>
+                </form>
+                <button form="formID" value="jeff" onclick="bababoey()" name="submit">submit</button>
 
-            <!-- Dranken -->
-            <label>Dranken: </label><br>
-            <select name="dranken" id="">
-                <option value="Water">Water</option>
-                <option value="Water">Water</option>
-                <option value="Water">Water</option>
-                <option value="Water">Water</option>
-                <option value="Water">Water</option>
-                <option value="Water">Water</option>
-            </select><br>
+                <!-- <button onclick="bababoey()" name="submit">Submit</button> -->
+                <!-- <input onclick="bababoey()" type="submit" name="submit"> -->
 
-        </form>
+                <!-- Reset button voor debug -->
+                <button><a href="index.php">Reset</a></button>
+                <!-- Overzicht anchor voor debug -->
+                <a href="overzicht.php">overzicht</a>
+            </div>
+        </div>
 
-        <button form="formID" value="jeff" onclick="bababoey()" name="submit">submit</button>
-        <!-- <button onclick="bababoey()" name="submit">Submit</button> -->
-        <!-- <input onclick="bababoey()" type="submit" name="submit"> -->
-
-        <!-- Reset button voor debug -->
-        <button><a href="index.php">Reset</a></button>
+        <div class="containerFOOTER">
+            <h1 class="footerHEADER">Elegante kapsalon</h1>
+        </div>
 
         <script src="scripts/js/main.js"></script>
     </body>
@@ -164,11 +174,28 @@
     $voorraad = ["Gehaktbal", "Kip", "Water"];
 
     function bestellen() {
-        $bestelling = ["voornaam" => $_GET["voornaam"],"achternaam" => $_GET["achternaam"],"email" => $_GET["email"],"straatnaam" => $_GET["straatnaam"],"huisnummer" => $_GET["huisnummer"],"postcode" => $_GET["postcode"],"woonplaats" => $_GET["woonplaats"],"telefoonNummer" => $_GET["telefoonNummer"],"bestelling" => $_GET["gerecht"],];
+        $bestelling = ["voornaam" => $_GET["voornaam"],"achternaam" => $_GET["achternaam"],"email" => $_GET["email"],"straatnaam" => $_GET["straatnaam"],"huisnummer" => $_GET["huisnummer"],"postcode" => $_GET["postcode"],"woonplaats" => $_GET["woonplaats"],"telefoonNummer" => $_GET["telefoonNummer"],"gerecht" => $_GET["gerecht"],"dranken" => $_GET["dranken"]];
+        echo ("balzak Mikael Ainalem");
 
+        $_SESSION = ["voornaam" => $_GET["voornaam"],"achternaam" => $_GET["achternaam"],"email" => $_GET["email"],"straatnaam" => $_GET["straatnaam"],"huisnummer" => $_GET["huisnummer"],"postcode" => $_GET["postcode"],"woonplaats" => $_GET["woonplaats"],"telefoonNummer" => $_GET["telefoonNummer"],"gerecht" => $_GET["gerecht"],"dranken" => $_GET["dranken"]];
 
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            foreach ($bestelling as $value) {
+                if ($bestelling[$value] == "voornaam") {
+                    echo $bestelling[$value];
+                }
+
+            }
+            /*
+            if($bestelling) {
+                header('location: overzicht.php');
+            }
+            */
+        }
+
+        //var_dump($bestelling );
+        //print_r($bestelling);
         //controlleEten($bestelling);
-
     }
 
     function controlleVoorraad($bestelling){
---
+++ overzicht.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="scripts/css/styles.css">
+    </head>
+
+    <body>
+        <div class="containerHEADER">
+            <h1 class="headerCLASS">Bestelling</h1>
+        </div>
+
+        <!-- debug -->
+        <div class="containerFORM">
+            <a href="index.php">index.php</a>
+        </div>
+
+        <div class="containerFOOTER">
+            <h1 class="footerOVERZICHT">Overzicht</h1>
+        </div>
+        <script src="scripts/js/main.js"></script>
+    </body>
+</html>
+
+<?php
+
+    var_dump($_SESSION);
+    //var_dump(bestelling);
+
+?>
